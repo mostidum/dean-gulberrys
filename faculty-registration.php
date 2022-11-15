1653f5b16f954dbae6a46d3abe6dacd601e6bd65
StavroG <60533114+StavroG@users.noreply.github.com>
2022-11-15 13:02:49 -0800

    Updated gradient background

--- faculty-registration.php
+++ faculty-registration.php
@@ -9,7 +9,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Faculty Registration</title>
-    <link rel="stylesheet" type="text/css" href="css/background.css">
 </head>
     <?php
         include('includes/nav.php');

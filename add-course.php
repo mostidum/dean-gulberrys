5567c313ebc0803e7da59cebc0ff00f0863117a6
StavroG <60533114+StavroG@users.noreply.github.com>
2022-11-15 00:45:19 -0800

    Updated the Course Add page

--- add-course.php
+++ add-course.php
@@ -8,57 +8,80 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Registration</title>
-    <link rel="stylesheet" href="css/styles.css">
-    <script type="text/javascript" src="../js/studentRegister.js"></script>
+    <title>Course Add</title>
 </head>
     <?php
         include('includes/nav.php');
         include('includes/dbh.php');
     ?>
-
     <body>
-        <div class="register-container">
-            <div class="register-form">
-                <h1>Add a new course</h1>
-                <form action="add-course-process.php" method="post">
-                    <label for="course-title">Course Title</label><br>
-                    <input type="text" id="course-title" name="course-title"><br>
-                    
-                    <label for="course-description">Course Description</label><br>
-                    <input type="text" id="course-description" name="course-description"><br>
-                    
-                    <label for="units">Units</label><br>
-                    <input type="number" id="units" name="units"><br>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Course Add
+                            <a href="index.php" class="btn btn-danger float-end">BACK</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="add-course-process.php" method="post">
+                            <div class="mb-2">
+                                <label>Course Title</label>
+                                <input type="text" name="course-title" class="form-control">
+                            </div>
+                            <div class="mb-2">
+                                <label>Course Description</label>
+                                <input type="text" name="course-description" class="form-control">
+                            </div>
+                            <div class="mb-2 w-25">
+                                <label>Units</label>
+                                <input type="number" name="units" class="form-control" max=999 min=0 placeholder="ex: 3">
+                            </div>
+                            <div class="mb-3">
+                                <label>Department</label>
+                                <select id="department" name="department" class="form-select">
+                                    <option></option>
+                                    <?php
+                                        $sql = "SELECT * FROM department";
+                                        $result = mysqli_query($conn, $sql);
+                                        while ($row = mysqli_fetch_array($result)) {
+                                            echo "<option>" . $row['department_name'] . "</option>";
+                                        }
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="mb-2">
+                                <label>Prerequisites</label>
+                                <input type="text" name="prerequisites" class="form-control">
+                            </div>
+                            <div class="mb-2 w-25">
+                                <label>Course Number</label>
+                                <input type="number" name="course-number" class="form-control" max=999 min=0 placeholder="ex: 80">
+                            </div>
+                            <div class="form-check mb-2">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" name="graduate">
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    Graduate Course
+                                </label>
+                            </div>
+                            
+                          
 
-                    <label for="department">Department</label><br>
-                    <select id="department" name="department">
-                        <?php
-                            $sql = "SELECT * FROM department";
-                            $result = mysqli_query($conn, $sql);
-                            while($row = mysqli_fetch_array($result)){
-                                echo "<option>" .$row['department_name']. "</option>";
-                            }
-                        ?>
 
-                    </select><br>
+                            <div class="mb-2">
+                                <button type="submit" name="submit" class="btn btn-primary">Add Course</button>
+                            </div>
 
-                    <label for="prerequisites">Prerequisites</label><br>
-                    <input type="text" id="prerequisites" name="prerequisites"><br>
-
-                    <label for="course-number">Course Number</label><br>
-                    <input type="number" id="course-number" name="course-number" min="10" max="999"><br>
-
-                    <label for="graduate">Is Graduate Course?</label>
-                    <input type="checkbox" id="graduate" name="graduate" placeholder="1 for graduate else 0"><br>
-
-                    <br><button type="submit" id="button" name="submit">Add Course</button>
-
-                    <p1 id="errorMessage"></p1>
-                </form>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
+
 
     <?php
         include('includes/footer.php');
